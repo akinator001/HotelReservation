da3b1dcb0b45a36e1b5987276f2eafe91fa03889
akinator001 <aakashgandhi1279@gmail.com>
2020-10-15 20:04:12 +0530

    [aakash] Added : find cheap hotel with different weekday and weekend rate

--- main/java/com/cp/hotelreservation/HotelReservation.java
+++ main/java/com/cp/hotelreservation/HotelReservation.java
@@ -3,6 +3,7 @@ package com.cp.hotelreservation;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
@@ -19,7 +20,7 @@ public class HotelReservation {
 	
 	public String findCheapestHotel(String startDate,String lastDate) throws ParseException {
 		checkin = new SimpleDateFormat("ddMMMyyyy").parse(startDate);
-		checkout = new SimpleDateFormat("ddMMMyyyy").parse(startDate);
+		checkout = new SimpleDateFormat("ddMMMyyyy").parse(lastDate);
 		
 		long days = ((checkout.getTime()-checkin.getTime())/(1000*60*60*24))+1;
 
@@ -30,5 +31,47 @@ public class HotelReservation {
 				                  .orElse(null);
 		System.out.println("Hotel : "+ cheapestHotel.getHotelName()+" Cost : "+ minRent);
 		return cheapestHotel.getHotelName();		
-	} 
+	}
+	
+	public List<String> findCheapestHotelWeekdayAndWeekend(String start, String finish) throws ParseException {
+		convertToDates(start, finish);
+		long days = ((checkout.getTime()-checkin.getTime())/(1000*60*60*24))+1;
+		
+		long weekendDays = getWeekendDays(checkin, checkout);
+		long weekDays = days - weekendDays;
+		
+		List<Long> hotelRentList = hotelList.stream()
+				                .map(hotel -> calculateHotelCost(hotel, weekDays, weekendDays)).collect(Collectors.toList());
+		long minRent = Collections.min(hotelRentList);
+		
+		List<String> cheapHotelList = hotelList.stream()
+				                   .filter(hotel -> calculateHotelCost(hotel, weekDays, weekendDays) == minRent)
+				                    .map(hotel -> hotel.getHotelName()).collect(Collectors.toList());
+		for (String hotel : cheapHotelList)
+			System.out.println("Hotel : " + hotel + " Cost : " + minRent);
+        return cheapHotelList;
+	}
+
+	public long calculateHotelCost(Hotel hotel, long weekDays, long weekendDays) {
+		return hotel.getRegularCustomerWeekdayRate() * weekDays + hotel.getRegularCustomerWeekendRate() * weekendDays;
+	}
+
+	private long getWeekendDays(Date checkin2, Date checkout2) {
+		long weekendDays = 0;
+		Calendar startCalendar = Calendar.getInstance();
+		Calendar endCalendar = Calendar.getInstance();
+		endCalendar.setTime(checkout);
+		for (startCalendar.setTime(checkin); startCalendar.compareTo(endCalendar) <= 0; startCalendar.add(Calendar.DATE, 1)) {
+			int dayOfWeek = startCalendar.get(Calendar.DAY_OF_WEEK);
+			if (dayOfWeek == 0 || dayOfWeek == 6)
+				weekendDays++;
+		}
+		return weekendDays;
+	}
+
+	private void convertToDates(String startDate, String lastDate) throws ParseException {
+		checkin = new SimpleDateFormat("ddMMMyyyy").parse(startDate);
+		checkout = new SimpleDateFormat("ddMMMyyyy").parse(lastDate);
+
+	}
 }
